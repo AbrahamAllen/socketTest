f341d2d2e4bbe03ec405b10093fe59f1076185bb
AbrahamAllen <100115719+AbrahamAllen@users.noreply.github.com>
2024-01-27 19:42:00 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
 const port = process.env.PORT || 3000;
-const express = require('express');
+
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
-io.on('connection', (socket) => {
-  socket.on('test', (msg) => {
-	  console.log(msg[0]);
-	io.emit('test', msg)
-  });
-});
-
-server.listen(port, () => {
-  console.log('listening on *:3000');
-});
+server.listen(port);
