1966fdd41ab3ee13efddcc9cd46669c8cc81f018
AbrahamAllen <100115719+AbrahamAllen@users.noreply.github.com>
2024-02-11 02:02:55 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-const port = 3000;
+const port = (process.env.PORT || 3000);
 const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  socket.on('test', (msg) => {
+	  console.log(msg[0]);
+	io.emit('test', msg)
+  });
+});
+
+server.listen(port, () => {
+  console.log(port);
+}); 
