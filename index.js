03d792620a938af626698a2cc000396430878251
AbrahamAllen <100115719+AbrahamAllen@users.noreply.github.com>
2024-02-12 06:40:21 -0800

    Update index.js

--- index.js
+++ index.js
@@ -6,14 +6,46 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+let map = {};
+let updater = {};
+
+class obj {
+  constructor(id, left, top) {
+	this.id = id;
+    this.left = left;
+    this.top = top;
+  }
+}
+
+
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-  socket.on('test', (msg) => {
-	  console.log(msg[0]);
-	io.emit('test', msg)
+  socket.on('newPlayer', function(){
+	  console.log(socket.id);
+	  let playerid = 'player'+socket.id;
+	  map[playerid] = new obj(playerid, 10, 10);
+	  io.emit('newPlayer', playerid);
+  });
+  
+  socket.on('input', function(data){
+	  try{
+		map[data[0]].left+=data[1];
+		map[data[0]].top+=data[2];
+		updater[data[0]] = map[data[0]];
+	  }catch{
+		  console.log(data);
+	  }
+  }); 
+  
+  setInterval(function(){io.emit('updateMap', updater); updater = {}},60);
+  
+  socket.on('disconnect', function(){
+	console.log(socket.id);
+	delete map['player'+socket.id] 
   });
 });
 
